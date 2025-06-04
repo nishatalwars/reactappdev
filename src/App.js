414e5bc5dbe9cd78e7432140bbc23187559df1f6
Nisha <nishakashyap342@gmail.com>
2025-06-04 11:24:47 +0530

    Update Code (V 0.0.2)
    Change in heading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
     
-<p>Hello Reactjs Developer this sides.</p>
+<p>Hello Reactjs junior Developer this sides.</p>
       </header>
     </div>
   );
